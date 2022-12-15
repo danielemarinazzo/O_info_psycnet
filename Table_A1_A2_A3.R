3badb316259d0a48c69186e7d5e8250bd5fb0459
Daniele Marinazzo <daniele.marinazzo@gmail.com>
2022-12-15 12:05:02 +0100

    print covariance

--- Table_A1_A2_A3.R
+++ Table_A1_A2_A3.R
@@ -6,30 +6,30 @@ library(qgraph)
 l1 <- sqrt(0.99)
 l2 <- sqrt(0.70)
 l3 <- sqrt(0.30)
-S.ecov <- -0.39 # synergy
-#Z.ecov <- -0.14849 # ~zero interaction information
-#R.ecov <- 0.22 # redundancy
+#A.ecov <- -0.39 # synergy
+#A.ecov <- -0.14849 # ~zero interaction information
+A.ecov <- 0.22 # redundancy
 t1 <- 1 - l1^2 #variance
 t2 <- 1 - l2^2
 t3 <- 1 - l3^2
 
-S.ecor <- S.ecov / (sqrt(t2) * sqrt(t3)) #correlation
-S.t2s <- t2 - abs(S.ecor)*t2 #used in lavaan syntax
-S.t3s <- t3 - abs(S.ecor)*t3 #used in lavaan syntax
-S.l2s <- 1 * sqrt(abs(S.ecor)*t2)
-S.l3s <- sign(S.ecor) * sqrt(abs(S.ecor)*t3)
+A.ecor <- A.ecov / (sqrt(t2) * sqrt(t3)) #correlation
+A.t2s <- t2 - abs(A.ecor)*t2 #used in lavaan syntax
+A.t3s <- t3 - abs(A.ecor)*t3 #used in lavaan syntax
+A.l2s <- 1 * sqrt(abs(A.ecor)*t2)
+A.l3s <- sign(A.ecor) * sqrt(abs(A.ecor)*t3)
 
 pop.model <- c("
-# model S
-S =~ (", l1, ")*S.x + (", l2, ")*S.y + (", l3, ")*S.z
-S.bf =~ (", S.l2s, ")*S.y + (", S.l3s, ")*S.z
-S.x ~~ (", t1, ")*S.x
-S.y ~~ (", S.t2s, ")*S.y
-S.z ~~ (", S.t3s, ")*S.z
+# model A
+A =~ (", l1, ")*A.x + (", l2, ")*A.y + (", l3, ")*A.z
+A.bf =~ (", A.l2s, ")*A.y + (", A.l3s, ")*A.z
+A.x ~~ (", t1, ")*A.x
+A.y ~~ (", A.t2s, ")*A.y
+A.z ~~ (", A.t3s, ")*A.z
 
-S ~~ 1*S
-S.bf ~~ 1*S.bf
-S ~~ 0*S.bf
+A ~~ 1*A
+A.bf ~~ 1*A.bf
+A ~~ 0*A.bf
 ")
 
 fit <- lavaan(pop.model)
@@ -39,4 +39,8 @@ COV <- lavInspect(fit, "implied")$cov
 graph_r1.g<-qgraph(COV, layout="circular", graph="glasso", 
                    sampleSize=2000, cut=0, theme="colorblind")
 #dev.off()
-
+PCOR <- -1 * cov2cor(solve(COV))
+diag(PCOR) <- 1
+print(COV)
+print(PCOR)
+print(COV^2 - PCOR^2)
